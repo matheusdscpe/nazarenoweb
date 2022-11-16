4780bacc4c7fafed65dac4af32a1c461f5c9c75e
Matheus Carvalho <matheuscarvalho.pe@hotmail.com>
2022-11-16 20:45:21 -0300

    adição de animação no menu lateral

---
+++ js/menu-mobile.js
@@ -0,0 +1,21 @@
+const botaoAbrirMenu = document.querySelector('#bnt-menu');
+const botaoFecharMenu = document.querySelector('#bnt-fechar-menu');
+const menu_lateral = document.querySelector('#menu-lateral');
+
+
+function abrirMenu(){
+    menu_lateral.classList.toggle("menu-lateral-aberto", "menu-lateral-fechado");
+    setTimeout(() => {botaoAbrirMenu.classList.toggle("remove-icone-hamburguer");}, 300);
+}
+
+function fecharMenu(){
+    menu_lateral.classList.toggle("fechar-menu-mobile");
+    botaoAbrirMenu.classList.toggle("remove-icone-hamburguer");
+    
+     setTimeout(() => {menu_lateral.classList.remove("menu-lateral-aberto", "fechar-menu-lateral");}, 500);
+}
+
+
+
+botaoAbrirMenu.addEventListener("click", abrirMenu);
+botaoFecharMenu.addEventListener("click", fecharMenu);
